2d33a7b6267294fa3f8fff41e63eeab9616d3e2e
borjadotai <hi@borja.ai>
2023-10-22 11:09:08 +0100

    add changes

---
+++ hangman/milestone_2.py
@@ -0,0 +1,17 @@
+import random
+
+word_list = ["banana", "apple", "cherries", "pear", "peach"]
+word = random.choice(word_list)
+print(word)
+
+guess = input()
+
+try:
+    if len(guess) > 1:
+        raise ValueError("The string is longer than one character.")
+    elif not guess.isalpha():
+        raise ValueError("The string is not alphabetical.")
+    else:
+        print("Good guess!")
+except ValueError as e:
+    print("Oops! That is not a valid input.")
